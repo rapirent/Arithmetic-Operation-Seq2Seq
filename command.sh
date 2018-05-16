f769fa21eff4cec0c5ace1d72b1779212b9a793e
kuoteng <rapirent@gmail.com>
2018-05-16 23:46:42 +0800

    500000data train

--- command.sh
+++ command.sh
@@ -24,7 +24,7 @@
 # python3 ./addition-subtractor.py "--epoch=2" "--output_name=digit5" "--data_size=75000" "--train_size=45000" "--digits=5"
 # python3 ./addition-subtractor.py "--epoch=2" "--output_name=digit4" "--data_size=75000" "--train_size=45000" "--digits=4"
 
-python3 ./addition-subtractor.py "--epoch=2" "--output_name=train60000" "--data_size=90000" "--train_size=60000" "--digits=3"
+# python3 ./addition-subtractor.py "--epoch=2" "--output_name=train60000" "--data_size=90000" "--train_size=60000" "--digits=3"
 # python3 ./subtractor.py "--epoch=2" "--output_name=train45000large" "--data_size=95000" "--train_size=45000" "--digits=3"
 # python3 ./subtractor.py "--epoch=2" "--output_name=digit4-train60000" "--data_size=90000" "--train_size=60000" "--digits=4"
 
@@ -38,7 +38,8 @@ python3 ./addition-subtractor.py "--epoch=2" "--output_name=train60000" "--data_
 
 # python3 ./addition-subtractor.py "--epoch=2" "--output_name=train90000" "--data_size=120000" "--train_size=90000" "--digits=3"
 
-python3 ./multiply.py "--epoch=2" "--output_name=train120000" "--data_size=150000" "--train_size=120000" "--digits=3"
+# python3 ./multiply.py "--epoch=2" "--output_name=train120000" "--data_size=150000" "--train_size=120000" "--digits=3"
+python3 ./multiply.py "--epoch=2" "--output_name=train1200000" "--data_size=530000" "--train_size=500000" "--digits=3"
 # python3 ./addition-subtractor.py "--epoch=2" "--output_name=digit4-train36000" "--data_size=66000" "--train_size=36000" "--digits=4"
 # python3 ./addition-subtractor.py "--epoch=2" "--output_name=digit4-train45000" "--data_size=75000" "--train_size=45000" "--digits=4"
 # python3 ./addition-subtractor.py "--epoch=2" "--output_name=digit4-train45000" "--data_size=90000" "--train_size=60000" "--digits=4"
